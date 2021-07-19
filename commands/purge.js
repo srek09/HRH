2c6226739132193a23588602331fc083919e141b
andras1109 <ignacz.andras1109@gmail.com>
2021-07-19 21:23:13 +0200

    nibbányvadász1.2

--- commands/purge.js
+++ commands/purge.js
@@ -17,13 +17,14 @@ module.exports = {
         if (!amountToDelete) return message.reply('Meg kell adnod, hogy hány üzenet kerüljön törlésre (+purge \'szám\'')
         if (!Number.isInteger(amountToDelete)) return message.reply('Ez nem egy egész szám!')
         if (amountToDelete < 2 || amountToDelete > 100) return message.reply('A számnak 2 és 100 között kell lennie!')
-        const fetched = message.channel.messages.fetch({
-            limit: amountToDelete
-        })
 
         // Executing:
         try {
-            await message.channel.bulkDelete(fetched).then(messages => message.channel.send(`${messages.size} üzenet törlésre került.`))
+            await message.channel.fetch({
+                limit: amountToDelete
+            }).then(messages => {
+                bulkDelete(messages)
+            })
         } catch (err) {
             console.log(err)
 
