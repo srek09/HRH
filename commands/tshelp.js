76fa8afceff637c88193d11f2875efb28a399348
andras1109 <ignacz.andras1109@gmail.com>
2021-07-19 20:41:51 +0200

    nibbányvadász1.0.2

--- commands/tshelp.js
+++ commands/tshelp.js
@@ -7,17 +7,18 @@ module.exports = {
     guildOnly: true,
     cooldown: 2,
     async execute(message, args) {
+
         // Variables:
         const embed = new Discord.MessageEmbed()
             .setColor('#1ad94d')
             .setTitle(`TeamSpeak szerverünk ip címe: hrh.zap-ts3.com`)
-            .setDescription('A szerveren kötelező a TeamSpeak használata!')
+            .setDescription('A szerverre való csatlakozáshoz a TeamSpeak szükséges!')
             .addFields({
                 name: "Szerverünkre a TeamSpeak 3-as klienssel lehet felcsatlakozni:",
-                value: "[letöltés](https://teamspeak.com/en/downloads/)"
+                value: "[innen töltheted le](https://teamspeak.com/en/downloads/)"
             }, {
                 name: "Szerverünkre a Tokovoip plugin 1.5.4-es verzió szükséges:",
-                value: "[letöltés](https://github.com/Itokoyamato/TokoVOIP_TS3/releases/download/v1.5.6/tokovoip-1.5.4.ts3_plugin)"
+                value: "[innen töltheted le](https://github.com/Itokoyamato/TokoVOIP_TS3/releases/download/v1.5.6/tokovoip-1.5.4.ts3_plugin)"
             })
             .setFooter('Ez a bot a HRH tulajdona, esetleges hibával/észrevétellel keresd: srek')
             .setThumbnail('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQxR5jSyrZQQo1gkRJk4VVfNcCTWpZbbLQew&usqp=CAU')
